f2da72bd3d0bc67870e385b07321381e1bc09791
nareshkumars <naresh.samesh@gmail.com>
2022-12-20 20:02:30 +0530

    updated configserver

--- insurance-provider/src/main/java/com/springcloud/insuranceprovider/controller/InsuranceProviderController.java
+++ insurance-provider/src/main/java/com/springcloud/insuranceprovider/controller/InsuranceProviderController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/getallplans")
+@RequestMapping("/getinsuranceplans")
 public class InsuranceProviderController {
 
     @GetMapping
